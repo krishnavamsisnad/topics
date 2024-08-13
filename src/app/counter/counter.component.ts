4c02e4d9df819056b905af88cba8e7bdc4e9ff40
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-08-13 15:40:24 +0530

    task

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IcOUNTER } from './counterr.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
 
-  counter!:IcOUNTER[]
+  counter:IcOUNTER[]=[]
+  counterId = 0;
+  @Output() 
   addcounter(){
-    
+    if (this.counter) {
+      this.counter.push({ id: this.counterId++, count: 0 });
+    }
+  }
+  increment(counter:IcOUNTER){
+    counter.count++
+  }
+  decrement(counter:IcOUNTER){
+    if(counter.count>0){
+      counter.count--
+    }
+
   }
 
 }

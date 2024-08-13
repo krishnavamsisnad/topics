f03b3e11d4fc710c018f6a834a40bdaf92612726
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-08-13 15:18:46 +0530

    task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
-import { CategoriesComponent } from './categories/categories.component';
-import { CounterComponent } from './counter/counter.component';
+
 
 export const routes: Routes = [
-    {path:'',component:CategoriesComponent},
-    {path:'counter',component:CounterComponent}
-];
+]

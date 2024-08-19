91986683dc8944949d5976ad227d6c08732919bd
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-08-19 15:41:38 +0530

    ss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from "./products/products.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductsComponent],
+  imports: [RouterOutlet, ProductsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
